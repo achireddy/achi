55d29859dcfb065a05b104e51e70e84cd0735949
Achi <kurriachi@gmail.com>
2018-11-18 21:22:56 -0600

    initial setting

---
+++ sampleWebApp99/sampleWebApp99/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sampleWebApp99.Global" Language="C#" %>
---
+++ sampleWebApp99/sampleWebApp99/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sampleWebApp99.Startup))]
+namespace sampleWebApp99
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
